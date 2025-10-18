d81ae81525b3cf548feed98efe79ad2a17b0cbf5
urvold <5ru9en@mail.ru>
2025-10-18 20:44:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-
-// Smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(a=>{
-  a.addEventListener('click', e=>{
-    const id = a.getAttribute('href');
-    if(id.length>1){
-      e.preventDefault();
-      document.querySelector(id).scrollIntoView({behavior:'smooth', block:'start'});
-    }
-  });
-});
-// Netlify honeypot
-const form = document.querySelector('form[name="lead"]');
-if(form){
-  const hp = document.createElement('input');
-  hp.type = 'text'; hp.name = 'company'; hp.style.display='none';
-  form.appendChild(hp);
-}
+document.querySelectorAll('a[href^="#"]').forEach(a=>{a.addEventListener('click',e=>{const id=a.getAttribute('href');if(id.length>1&&document.querySelector(id)){e.preventDefault();document.querySelector(id).scrollIntoView({behavior:'smooth'});}})});
